f10984ebef9e03d82c0ba8fb9544bb3e598026b3
Nahir Veizaga <nahirv@uvic.ca>
2024-11-28 11:09:11 -0800

    view notes functionality

--- app.py
+++ app.py
@@ -70,12 +70,12 @@ class ERAdmission(db.Model):
     medication = db.Column(db.String, nullable=True)
     timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
 
-class VisitNote(db.Model):
+class PhysicianNote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    patient_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    er_admission_id = db.Column(db.Integer, db.ForeignKey('er_admission.id'), nullable=False)
     physician_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    admission_id = db.Column(db.Integer, db.ForeignKey('er_admission.id'), nullable=False)
-    note = db.Column(db.Text, nullable=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    note_text = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
 
 # makes the input for username and password
@@ -423,7 +423,7 @@ def triage_form():
 @app.route('/view_notes', methods=['GET', 'POST'])
 @login_required
 def view_notes():
-    visit_notes = VisitNote.query.filter_by(patient_id=current_user.id).all()
+    visit_notes = PhysicianNote.query.filter_by(patient_id=current_user.id).all()
     return render_template('view_notes.html', visit_notes=visit_notes)
 
 #route for ping/echo
